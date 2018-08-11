726f3636cf634a59cde0e0a4c4f008089821945a
Jcontratto <jmc22@hotmail.com>
2018-08-11 19:21:56 -0400

    another commit

--- liri.js
+++ liri.js
@@ -1,3 +1,142 @@
+// Required NPM modules and files.
+// ____________________________________________________________________________________
+
+// NPM module used to access Twitter API.
+var Twitter = require("twitter");
+
+// Used to access Twitter keys in local file, keys.js.
+var twitterKeysFile = require("./keys.js");
+
+// NPM module used to access Spotify API.
+var spotify = require("spotify");
+
+// NPM module used to access OMDB API.
+var request = require("request");
+
+// NPM module used to read the random.txt file.
+var fs = require("fs");
+
+// Output file for logs.
+var filename = './log.txt';
+
+// NPM module used for logging solution.
+var log = require('simple-node-logger').createSimpleFileLogger( filename );
+
+// All log information printed to log.txt.
+log.setLevel('all');
+
+// Controller and required parameters.
+// ____________________________________________________________________________________
+
+
+// Action requested.
+var action = process.argv[2];
+
+// Optional argument to request specific information.
+// Based on action type.
+var argument = "";
+
+// Controller function that determines what action is taken,
+// and specific data to complete that action.
+doSomething(action, argument);
+
+// Switch operation used to determin which action to take.
+function doSomething(action, argument) {
+
+	/* Controls optional third argument.
+	Defines specific data relating to the action.
+	For example, when requesting song information,
+	you can pass in a song title.
+	*/ 
+	argument = getThirdArgument();
+
+	switch (action) {
+		
+		// Gets list of tweets.
+		case "my-tweets": 
+		getMyTweets();
+		break;
+
+		// Gets song information.
+		case "spotify-this-song":
+		
+		// First gets song title argument.
+		var songTitle = argument;
+
+		// If no song title provided, defaults to specific song.
+		if (songTitle === "") {
+			lookupSpecificSong();
+
+		// Else looks up song based on song title.
+		} else {
+			// Get song information from Spotify.
+			getSongInfo(songTitle);
+		}
+		break;
+
+		// Gets movie information.
+		case "movie-this":
+
+		// First gets movie title argument.
+		var movieTitle = argument;
+
+		// If no movie title provided, defaults to specific movie.
+		if (movieTitle === "") {
+			getMovieInfo("Mr. Nobody");
+
+		// Else looks up song based on movie title.
+		} else {
+			getMovieInfo(movieTitle);
+		}
+		break;
+
+		// Gets text inside file, and uses it to do something.
+		case "do-what-it-says": 
+		doWhatItSays();
+		break;
+	}
+}
+
+// Returns optional third argument, for example,
+// when requesting song information, include a song title.
+function getThirdArgument() {
+
+	// Stores all possible arguments in array.
+	argumentArray = process.argv;
+
+	// Loops through words in node argument.
+	for (var i = 3; i < argumentArray.length; i++) {
+		argument += argumentArray[i];
+	}
+	return argument;
+}
+
+// Function to show my last 20 tweets.
+function getMyTweets() {
+	
+	// Passes Twitter keys into call to Twitter API.
+	var client = new Twitter(twitterKeysFile.twitterKeys);
+
+	// Search parameters includes my tweets up to last 20 tweets;
+	var params = {q: '@john97301035', count: 20};
+
+	// Shows up to last 20 tweets and when created in terminal.
+	client.get('search/tweets', params, function(error, tweets, response) {
+	  if (!error) {
+
+	  	// Loops through tweets and prints out tweet text and creation date.
+	  	for (var i = 0; i < tweets.statuses.length; i++) {
+	  		var tweetText = tweets.statuses[i].text;
+	  		logOutput("Tweet Text: " + tweetText);
+	  		var tweetCreationDate = tweets.statuses[i].created_at;
+	  		logOutput("Tweet Creation Date: " + tweetCreationDate);
+	  	}
+	  } else {
+	  	logOutput(error);
+	  }
+	});
+}
+
 // Calls Spotify API to retrieve song information for song title.
 function getSongInfo(songTitle) {
 
@@ -8,13 +147,7 @@ function getSongInfo(songTitle) {
 			return
 		}
 
-		/* The Spotify node module defaults to 20 no matter what.
-		Attempted to add a limit, which seems to do nothing.
-		Homework requirements suggest we should only return one song.
-		Used array properties to retrict songs returns.
-		There could very well be a better way to do this.
-		But it's as close to requirements I could get using Spotify module.
-		*/
+		
 		var artistsArray = data.tracks.items[0].album.artists;
 
 		// Array to hold artist names, when more than one artist exists for a song.
@@ -61,6 +194,8 @@ function getMovieInfo(movieTitle) {
 
 	// Runs a request to the OMDB API with the movie specified.
 	var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&tomatoes=true&r=json";
+
+	//http://www.imdb.com/title/tt0485947/
 
 	request(queryUrl, function(error, response, body) {
 	  // If the request is successful...
